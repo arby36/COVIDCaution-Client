dc2d886d51ac0cc0d6e78eaa05814bb1a776049a
Tejas Iyer <tejas.iyer@outlook.com>
2020-07-11 11:02:29 -0400

    App v1

--- app/src/main/java/com/hack3atx/covidcaution/MainActivity.java
+++ app/src/main/java/com/hack3atx/covidcaution/MainActivity.java
@@ -1,6 +1,9 @@
 package com.hack3atx.covidcaution;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -11,6 +14,14 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String name, email, password;
+
+    EditText nameInput;
+    EditText emailInput;
+    EditText passwordInput;
+
+    Button submitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +36,21 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+        nameInput = (EditText) findViewById(R.id.nameInput);
+        emailInput = (EditText) findViewById(R.id.emailInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+
+        submitButton = (Button) findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                name = nameInput.getText().toString();
+                email = emailInput.getText().toString();
+                password = passwordInput.getText().toString();
+            }
+        });
+
+    }
 }
