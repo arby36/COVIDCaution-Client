4276c29b9d3d206ced006014c1b4ed74a3453214
Tejas Iyer <tejas.iyer@outlook.com>
2020-07-11 15:09:17 -0400

    App v2

--- app/src/main/java/com/hack3atx/covidcaution/MainActivity.java
+++ app/src/main/java/com/hack3atx/covidcaution/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hack3atx.covidcaution;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     EditText passwordInput;
 
     Button submitButton;
+
+    public static final String SHARED_PREFS = "sharedPrefs";
+    public static final String NAME = "name";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +40,21 @@ public class MainActivity extends AppCompatActivity {
                 email = emailInput.getText().toString();
                 password = passwordInput.getText().toString();
 
+                saveData();
+
                 Intent intent = new Intent(MainActivity.this, MainActivity2.class);
                 startActivity(intent);
             }
         });
-//iqeoqiwjeqwklejqwlkejwq//
+    }
+
+    public void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(NAME, nameView.getName().toString());
+    }
+
+    public void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
     }
 }
